ca4f2c5e2d7e852caf2621e87b3650acccb6c8bc
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-06-05 10:48:59 +0500

    add

---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+
+    @Test
+    void checkTotalTest(){
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5));
+
+    }
+}
