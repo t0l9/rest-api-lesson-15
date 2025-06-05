bda0cb14c54ec0c7bf7a573635bf74cba44bc92d
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-06-05 17:15:36 +0500

    add tests login

--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class StatusTests {
@@ -14,6 +15,20 @@ public class StatusTests {
                 .then()
                 .log().all()
                 .body("total", is(5));
+
+    }
+
+    @Test
+    void checkTotalTest2(){
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("127.0"));
 
     }
 }
